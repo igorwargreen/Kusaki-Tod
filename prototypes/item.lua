63f71b6ccbeb75ae2e516c2e1b46b18a7acf2c06
igorwargreen <igorwargreen@gmail.com>
2024-12-14 11:19:35 +0300

    loader cost rebalance

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -42,6 +42,27 @@ data.raw["loader-1x1"]["aai-loader"].speed = 0.05
 data.raw["loader-1x1"]["aai-fast-loader"].speed = 0.125
 data.raw["loader-1x1"]["aai-express-loader"].speed = 0.25
 data.raw["loader-1x1"]["aai-turbo-loader"].speed = 0.5
+data.raw["recipe"]["aai-loader"].ingredients =
+    {
+      {type = "item", name = "transport-belt", amount = 1},
+      {type = "item", name = "electronic-circuit", amount = 1},
+      {type = "item", name = "iron-gear-wheel", amount = 1}
+    }
+data.raw["recipe"]["aai-fast-loader"].ingredients =
+    {
+      {type = "item", name = "aai-loader", amount = 1},
+      {type = "item", name = "advanced-circuit", amount = 1}
+    }
+data.raw["recipe"]["aai-express-loader"].ingredients =
+    {
+      {type = "item", name = "aai-fast-loader", amount = 1},
+      {type = "item", name = "processing-unit", amount = 1}
+    }
+data.raw["recipe"]["aai-turbo-loader"].ingredients =
+    {
+      {type = "item", name = "aai-express-loader", amount = 1},
+      {type = "item", name = "processing-unit", amount = 5}
+    }
 data.raw["underground-belt"]["underground-belt"].speed = 0.05
 data.raw["underground-belt"]["fast-underground-belt"].speed = 0.125
 data.raw["underground-belt"]["express-underground-belt"].speed = 0.25
