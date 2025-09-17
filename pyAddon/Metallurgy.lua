a19ad298e98c5dd66bf638a2261ace182a37397d
Abobus <igorwargreen@gmail.com>
2025-09-17 08:58:05 +0300

    Сборщики и литейные

---
+++ pyAddon/Metallurgy.lua
@@ -0,0 +1,19 @@
+data.raw["furnace"]["stone-furnace"].crafting_speed = 0.25
+data.raw["furnace"]["steel-furnace"].crafting_speed = 0.5
+data.raw["furnace"]["electric-furnace"].crafting_speed = 1
+
+data.raw["assembling-machine"]["advanced-foundry-mk01"].crafting_speed = 2
+data.raw["assembling-machine"]["advanced-foundry-mk02"].crafting_speed = 4
+data.raw["assembling-machine"]["advanced-foundry-mk03"].crafting_speed = 6
+data.raw["assembling-machine"]["advanced-foundry-mk04"].crafting_speed = 8
+
+data.raw["assembling-machine"]["assembling-machine-1"].crafting_speed = 0.5
+data.raw["assembling-machine"]["assembling-machine-2"].crafting_speed = 0.75
+data.raw["assembling-machine"]["assembling-machine-3"].crafting_speed = 1
+
+data.raw["assembling-machine"]["automated-factory-mk01"].crafting_speed = 2
+data.raw["assembling-machine"]["automated-factory-mk02"].crafting_speed = 4
+data.raw["assembling-machine"]["automated-factory-mk03"].crafting_speed = 6
+data.raw["assembling-machine"]["automated-factory-mk04"].crafting_speed = 8
+
+
