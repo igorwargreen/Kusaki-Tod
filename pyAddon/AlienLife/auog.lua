419921dde83449eca0c9373b536aa6bcf7faa2e8
Abobus <igorwargreen@gmail.com>
2025-09-18 09:54:08 +0300

    Ауги

--- pyAddon/AlienLife/auog.lua
+++ pyAddon/AlienLife/auog.lua
@@ -1,91 +1,23 @@
---
---recipes-auog-modules.lua
---
-data.raw["recipe"]["auog-mk02-breeder"].results = {
-		{type = "item", name = "auog-mk02",    amount = 2},
-        {type = "item", name = "auog-food-02", amount = 2},
-        {type = "item", name = "bedding",      amount = 4},
-        {type = "item", name = "moss",         amount = 10},
-        {type = "item", name = "saps",         amount = 10}
-	}
-data.raw["recipe"]["auog-mk02-breeder"].ingredients = {
-		{type = "item", name = "auog-pup-mk02", amount = 5}
-	}
-data.raw["recipe"]["auog-pup-mk02-breeder"].results = {
-		{type = "item", name = "auog-pup-mk02", amount = 4},
-        {type = "item", name = "auog-food-02",  amount = 2},
-        {type = "item", name = "bedding",       amount = 4},
-        {type = "item", name = "seaweed",       amount = 10},
-        {type = "item", name = "saps",          amount = 10},
-        {type = "item", name = "barrel-milk",   amount = 2}
-	}
-data.raw["recipe"]["auog-pup-mk02-breeder"].ingredients = {
-		{type = "item", name = "auog-mk02",         amount_min = 2, amount_max = 4},
-        {type = "item", name = "empty-barrel-milk", amount = 2}
-	}
-data.raw["recipe"]["auog-mk03-breeder"].results = {
-		{type = "item", name = "auog-mk03",    amount = 2},
-        {type = "item", name = "auog-food-02", amount = 2},
-        {type = "item", name = "bedding",      amount = 4},
-        {type = "item", name = "moss",         amount = 10},
-        {type = "item", name = "saps",         amount = 10}
-	}
-data.raw["recipe"]["auog-mk03-breeder"].ingredients = {
-		{type = "item", name = "auog-pup-mk03", amount = 5}
-	}
-data.raw["recipe"]["auog-pup-mk03-breeder"].results = {
-		{type = "item", name = "auog-pup-mk03", amount = 4},
-        {type = "item", name = "auog-food-02",  amount = 2},
-        {type = "item", name = "bedding",       amount = 4},
-        {type = "item", name = "seaweed",       amount = 10},
-        {type = "item", name = "saps",          amount = 10},
-        {type = "item", name = "barrel-milk",   amount = 2}
-	}
-data.raw["recipe"]["auog-pup-mk03-breeder"].ingredients = {
-		{type = "item", name = "auog-mk03",         amount_min = 1, amount_max = 4},
-        {type = "item", name = "empty-barrel-milk", amount = 2}
-	}
-data.raw["recipe"]["auog-mk04-breeder"].results = {
-		{type = "item", name = "auog-mk04",    amount = 2},
-        {type = "item", name = "auog-food-02", amount = 2},
-        {type = "item", name = "bedding",      amount = 4},
-        {type = "item", name = "moss",         amount = 10},
-        {type = "item", name = "saps",         amount = 10}
-	}
-data.raw["recipe"]["auog-mk04-breeder"].ingredients = {
-		{type = "item", name = "auog-pup-mk04", amount = 5}
-	}
-data.raw["recipe"]["auog-pup-mk04-breeder"].results = {
-		{type = "item", name = "auog-pup-mk04", amount = 4},
-        {type = "item", name = "auog-food-02",  amount = 2},
-        {type = "item", name = "bedding",       amount = 4},
-        {type = "item", name = "seaweed",       amount = 10},
-        {type = "item", name = "saps",          amount = 10},
-        {type = "item", name = "barrel-milk",   amount = 2}
-	}
-data.raw["recipe"]["auog-pup-mk04-breeder"].ingredients = {
-		{type = "item", name = "auog-mk04",         amount_min = 1, amount_max = 4},
-        {type = "item", name = "empty-barrel-milk", amount = 2}
-	}
+
 --
 --recipes-auog-products.lua
 --
-data.raw["recipe"]["auog-pooping-1"].results = {
+data.raw["recipe"]["auog-pooping-1"].ingredients = {
 		{type = "item", name = "native-flora", amount = 20},
 		{type = "item", name = "moss",         amount = 10}
 	}
-data.raw["recipe"]["auog-pooping-1"].ingredients = {
+data.raw["recipe"]["auog-pooping-1"].results = {
 		{type = "item", name = "manure", amount = 5}
 	}
-data.raw["recipe"]["auog-pooping-2"].results = {
+data.raw["recipe"]["auog-pooping-2"].ingredients = {
 		{type = "item", name = "native-flora", amount = 20},
 		{type = "item", name = "moss",         amount = 10},
 		{type = "item", name = "auog-food-01", amount = 3}
 	}
-data.raw["recipe"]["auog-pooping-2"].ingredients = {
+data.raw["recipe"]["auog-pooping-2"].results = {
 		{type = "item", name = "manure", amount = 10}
 	}
-data.raw["recipe"]["auog-pooping-3"].results = {
+data.raw["recipe"]["auog-pooping-3"].ingredients = {
 		{type = "item", name = "native-flora", amount = 20},
 		{type = "item", name = "moss",         amount = 10},
 		{type = "item", name = "auog-food-01", amount = 3},
@@ -93,52 +25,50 @@ data.raw["recipe"]["auog-pooping-3"].results = {
 		{type = "item", name = "bedding",   amount = 3},
 		{type = "item", name = "raw-fiber", amount = 15}
 	}
-data.raw["recipe"]["auog-pooping-3"].ingredients = {
+data.raw["recipe"]["auog-pooping-3"].results = {
 		{type = "item", name = "manure", amount = 15}
 	}
-data.raw["recipe"]["auog-pooping-4"].results = {
+data.raw["recipe"]["auog-pooping-4"].ingredients = {
 		{type = "item", name = "native-flora", amount = 20},
 		{type = "item", name = "moss",         amount = 10},
 		{type = "item", name = "saps",      amount = 17},
-		{type = "item", name = "bedding",   amount = 3},
+		{type = "item", name = "bedding",   amount = 8},
 		{type = "item", name = "raw-fiber", amount = 20},
-		{type = "item", name = "auog-food-02", amount = 5},
-		{type = "item", name = "bedding",      add_amount = 5}
+		{type = "item", name = "auog-food-02", amount = 5}
 	}
-data.raw["recipe"]["auog-pooping-4"].ingredients = {
+data.raw["recipe"]["auog-pooping-4"].results = {
 		{type = "item", name = "manure", amount = 20}
 	}
-data.raw["recipe"]["auog-pooping-5"].results = {
+data.raw["recipe"]["auog-pooping-5"].ingredients = {
 		{type = "item", name = "native-flora", amount = 20},
 		{type = "item", name = "moss",         amount = 10},
 		{type = "item", name = "saps",      amount = 27},
-		{type = "item", name = "bedding",   amount = 3},
+		{type = "item", name = "bedding",   amount = 8},
 		{type = "item", name = "raw-fiber", amount = 30},
-		{type = "item", name = "auog-food-02", amount = 5},
-		{type = "item", name = "bedding",      add_amount = 5}
+		{type = "item", name = "auog-food-02", amount = 5}
 	}
-data.raw["recipe"]["auog-pooping-5"].ingredients = {
+data.raw["recipe"]["auog-pooping-5"].results = {
 		{type = "item", name = "manure", amount = 27}
 	}
 --
 --recipes-auog-raising.lua
 --
-data.raw["recipe"]["auog-pup-breeding-1"].results = {
+data.raw["recipe"]["auog-pup-breeding-1"].ingredients = {
 		{type = "item", name = "native-flora", amount = 20},
 		{type = "item", name = "moss",         amount = 10}
 	}
-data.raw["recipe"]["auog-pup-breeding-1"].ingredients = {
+data.raw["recipe"]["auog-pup-breeding-1"].results = {
 		{type = "item", name = "auog-pup", amount = 3}
 	}
-data.raw["recipe"]["auog-pup-breeding-2"].results = {
+data.raw["recipe"]["auog-pup-breeding-2"].ingredients = {
 		{type = "item", name = "native-flora", amount = 20},
 		{type = "item", name = "moss",         amount = 10},
 		{type = "item", name = "auog-food-01", amount = 3}
 	}
-data.raw["recipe"]["auog-pup-breeding-2"].ingredients = {
+data.raw["recipe"]["auog-pup-breeding-2"].results = {
 		{type = "item", name = "auog-pup", amount = 6}
 	}
-data.raw["recipe"]["auog-pup-breeding-3"].results = {
+data.raw["recipe"]["auog-pup-breeding-3"].ingredients = {
 		{type = "item", name = "native-flora", amount = 20},
 		{type = "item", name = "moss",         amount = 10},
 		{type = "item", name = "auog-food-01", amount = 3},
@@ -146,10 +76,10 @@ data.raw["recipe"]["auog-pup-breeding-3"].results = {
 		{type = "item", name = "bedding",   amount = 3},
 		{type = "item", name = "raw-fiber", amount = 15}
 	}
-data.raw["recipe"]["auog-pup-breeding-3"].ingredients = {
+data.raw["recipe"]["auog-pup-breeding-3"].results = {
 		{type = "item", name = "auog-pup", amount = 12}
 	}
-data.raw["recipe"]["auog-pup-breeding-4"].results = {
+data.raw["recipe"]["auog-pup-breeding-4"].ingredients = {
 		{type = "item", name = "native-flora", amount = 20},
 		{type = "item", name = "moss",         amount = 10},
 		{type = "item", name = "auog-food-01", amount = 10},
@@ -158,10 +88,10 @@ data.raw["recipe"]["auog-pup-breeding-4"].results = {
 		{type = "item", name = "raw-fiber", amount = 20},
 		{type = "item", name = "auog-food-02", amount = 5}
 	}
-data.raw["recipe"]["auog-pup-breeding-4"].ingredients = {
+data.raw["recipe"]["auog-pup-breeding-4"].results = {
 		{type = "item", name = "auog-pup", amount = 19}
 	}
-data.raw["recipe"]["auog-pup-breeding-5"].results = {
+data.raw["recipe"]["auog-pup-breeding-5"].ingredients = {
 		{type = "item", name = "native-flora", amount = 20},
 		{type = "item", name = "moss",         amount = 10},
 		{type = "item", name = "auog-food-01", amount = 10},
@@ -170,30 +100,30 @@ data.raw["recipe"]["auog-pup-breeding-5"].results = {
 		{type = "item", name = "raw-fiber", amount = 30},
 		{type = "item", name = "auog-food-02", amount = 5}
 	}
-data.raw["recipe"]["auog-pup-breeding-5"].ingredients = {
+data.raw["recipe"]["auog-pup-breeding-5"].results = {
 		{type = "item", name = "auog-pup", amount = 32}
 	}
 
 
-data.raw["recipe"]["auog-maturing-1"].results = {
+data.raw["recipe"]["auog-maturing-1"].ingredients = {
 		{type = "item", name = "auog-pup",     amount = 4},
 		{type = "item", name = "native-flora", amount = 20},
 		{type = "item", name = "moss",         amount = 10}
 	}
-data.raw["recipe"]["auog-maturing-1"].ingredients = {
+data.raw["recipe"]["auog-maturing-1"].results = {
 		{type = "item", name = "auog", amount = 3}
 	}
-data.raw["recipe"]["auog-maturing-2"].results = {
+data.raw["recipe"]["auog-maturing-2"].ingredients = {
 		{type = "item", name = "auog-pup",     amount = 8},
 		{type = "item", name = "native-flora", amount = 20},
 		{type = "item", name = "moss",         amount = 10},
 		{type = "item", name = "auog-food-01", amount = 3},
 		{type = "item", name = "saps",         amount = 12}
 	}
-data.raw["recipe"]["auog-maturing-2"].ingredients = {
+data.raw["recipe"]["auog-maturing-2"].results = {
 		{type = "item", name = "auog", amount = 5}
 	}
-data.raw["recipe"]["auog-maturing-3"].results = {
+data.raw["recipe"]["auog-maturing-3"].ingredients = {
 		{type = "item", name = "auog-pup",     amount = 12},
 		{type = "item", name = "native-flora", amount = 20},
 		{type = "item", name = "moss",         amount = 10},
@@ -202,10 +132,10 @@ data.raw["recipe"]["auog-maturing-3"].results = {
 		{type = "item", name = "bedding",   amount = 3},
 		{type = "item", name = "raw-fiber", amount = 15}
 	}
-data.raw["recipe"]["auog-maturing-3"].ingredients = {
+data.raw["recipe"]["auog-maturing-3"].results = {
 		{type = "item", name = "auog", amount = 9}
 	}
-data.raw["recipe"]["auog-maturing-4"].results = {
+data.raw["recipe"]["auog-maturing-4"].ingredients = {
 		{type = "item", name = "auog-pup",     amount = 16},
 		{type = "item", name = "native-flora", amount = 20},
 		{type = "item", name = "moss",         amount = 10},
@@ -215,10 +145,10 @@ data.raw["recipe"]["auog-maturing-4"].results = {
 		{type = "item", name = "raw-fiber", amount = 20},
 		{type = "item", name = "auog-food-02", amount = 5}
 	}
-data.raw["recipe"]["auog-maturing-4"].ingredients = {
+data.raw["recipe"]["auog-maturing-4"].results = {
 		{type = "item", name = "auog", amount = 13}
 	}
-data.raw["recipe"]["auog-maturing-5"].results = {
+data.raw["recipe"]["auog-maturing-5"].ingredients = {
 		{type = "item", name = "auog-pup",     amount = 20},
 		{type = "item", name = "native-flora", amount = 20},
 		{type = "item", name = "moss",         amount = 10},
@@ -228,7 +158,7 @@ data.raw["recipe"]["auog-maturing-5"].results = {
 		{type = "item", name = "raw-fiber", amount = 30},
 		{type = "item", name = "auog-food-02", amount = 5}
 	}
-data.raw["recipe"]["auog-maturing-5"].ingredients = {
+data.raw["recipe"]["auog-maturing-5"].results = {
 		{type = "item", name = "auog", amount = 18}
 	}
 --
@@ -242,10 +172,10 @@ local BASE_GUTS = 3
 local BASE_BLOOD = 100
 local BASE_BRAIN = 1
 
-data.raw["recipe"]["full-render-auogs"].results = {
+data.raw["recipe"]["full-render-auogs"].ingredients = {
 		{type = "item", name = "auog",     amount = 1}
 	}
-data.raw["recipe"]["full-render-auogs"].ingredients = {
+data.raw["recipe"]["full-render-auogs"].results = {
 		{type = "item", name = "bones",       amount = BASE_BONES},
 		{type = "item", name = "meat",        amount = BASE_MEAT},
 		{type = "item", name = "skin",        amount = BASE_SKIN},
